c7eb2e7a8d0d417673f27ab923308c06b43d9251
YGJ <1392204404@qq.com>
2018-12-26 23:14:38 +0800

    使用Django内置的auth视图中的login函数进行登录登出以及判断用户是否处于登录状态

--- account/urls.py
+++ account/urls.py
@@ -9,5 +9,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
    # url(r'^login/$', views.user_login, name="user_login"), #自定义的登录
     url(r'^login/$', auth_views.login, name="user_login"),  #django内置的登录
-    url(r'^new-login/$', auth_views.login, {"template_name": "account/login.html"}),
+   # url(r'^new-login/$', auth_views.login, {"template_name": "account/login.html"}), #可以直接指定模板的名字
+    url(r'^logout/$', auth_views.logout, {"template_name": "account/logout.html"}, name="user_logout"),
 ]
