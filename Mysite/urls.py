355cc417244d22ccfa34664b875d7d3f826d7bee
YGJ <1392204404@qq.com>
2018-12-30 00:02:59 +0800

    栏目管理功能——显示和添加栏目

--- Mysite/urls.py
+++ Mysite/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url,include
+from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include(('blog.urls', 'blog'), namespace='blog')),
     url(r'^account/', include(('account.urls', 'account'), namespace='account')),
+    url(r'^article/',include(('article.urls','article' ), namespace='article')),
 ]
